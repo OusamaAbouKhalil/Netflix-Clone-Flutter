e822733de6cfe9e5cd506b14270510762dfaf328
Ousama Abou Khalil <OusamaAbouKhalil@gmail.com>
2023-11-28 16:19:22 +0200

    Adding some assets

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -11,10 +11,56 @@ class HomePageScreen extends StatefulWidget {
 class _HomePageScreenState extends State<HomePageScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: mobileBackgroundColor,
-      body: Center(
-        child: Text('Home Page'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: mobileBackgroundColor,
+            leading: Image.asset(
+              'assets/images/netflix.png',
+              scale: 20.0,
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'TV Shows',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Movies',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'My List',
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    )),
+              ),
+            ],
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 250.0,
+              child: Image.asset(
+                'assets/images/bg.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
